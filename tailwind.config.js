3d6d353cad0b6e8ec6b6891275cb010726aa1d49
PurpleFishh <asmarandei.catalin@yahoo.com>
2024-03-16 13:35:59 +0200

    Finised the CV

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,13 +8,18 @@ module.exports = {
       'accent' : '#A45179',
       'bg' : '#F1FFF8',
       'text' : '#1F0519',
-      'java-lang' : '#EE8B60',
+      'java-lang' : '#de3f23',
+      'html-lang' : '#EE8B60',
+      'css-lang' : '#7688b3',
+      'cpp-lang' : '#46c9e0',
+      'sqlite-lang' : '#0a4b6e',
     },
     extend: {
       backgroundImage: {
         'bg-main' : "url('../src/resorce/BG_Main_Image.svg')",
         'bg-projects' : "url('../src/resorce/BG_Projects_Image.svg')",
         'bg-skills' : "url('../src/resorce/BG_Skills_Image.svg')",
+        'bg-about' : "url('../src/resorce/BG_About_Image.svg')",
       }
     },
     fontFamily: {
